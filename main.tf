95a637688ac17692847c7e670dc78bdb9b807bd8
hariprasad.allaka@playngo.com <hariprasad.allaka@playngo.com>
2019-10-18 15:40:22 +0200

    Removing count on data sources

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 # Get object aws_vpc by vpc_id
 data "aws_vpc" "default" {
-  count = var.enabled ? 1 : 0
   id = var.vpc_id
 }
 
 data "aws_availability_zones" "available" {
-  count = var.enabled ? 1 : 0
 }
